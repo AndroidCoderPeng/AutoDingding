3aa3c245e19e6088ee213a47509f9ddae7062aa2
Pengxh <290677893@qq.com>
2022-12-20 08:55:26 +0800

    feat(*): 添加企业微信打卡设置界面，功能未完

---
+++ app/src/main/java/com/pengxh/autodingding/fragment/WeChatFragment.java
@@ -0,0 +1,21 @@
+package com.pengxh.autodingding.fragment;
+
+import com.pengxh.androidx.lite.base.AndroidxBaseFragment;
+import com.pengxh.autodingding.databinding.FragmentWechatBinding;
+
+public class WeChatFragment extends AndroidxBaseFragment<FragmentWechatBinding> {
+    @Override
+    protected void setupTopBarLayout() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initEvent() {
+
+    }
+}
