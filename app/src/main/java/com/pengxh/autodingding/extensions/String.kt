5d66f3f1b87ae85e0eea22e9ed1f1f2d6b20afc5
pengxianhong <290677893@qq.com>
2023-09-01 23:51:40 +0800

    perf(*): 优化代码，解决无法发送邮件的问题

--- app/src/main/java/com/pengxh/autodingding/extensions/String.kt
+++ app/src/main/java/com/pengxh/autodingding/extensions/String.kt
@@ -1,8 +1,11 @@
 package com.pengxh.autodingding.extensions
 
+import com.pengxh.autodingding.bean.MailInfo
+import com.pengxh.kt.lite.extensions.timestampToDate
 import java.text.ParseException
 import java.text.SimpleDateFormat
-import java.util.*
+import java.util.Calendar
+import java.util.Locale
 import kotlin.math.abs
 
 fun String.convertToWeek(): String {
@@ -48,4 +51,24 @@ fun String.diffCurrentMillis(): Long {
     val simpleDateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.CHINA)
     val date = simpleDateFormat.parse(this)!!
     return abs(System.currentTimeMillis() - date.time)
+}
+
+fun String.createMail(toAddress: String): MailInfo {
+    val mailInfo = MailInfo()
+    mailInfo.mailServerHost = "smtp.qq.com" //发送方邮箱服务器
+    mailInfo.mailServerPort = "587" //发送方邮箱端口号
+    mailInfo.isValidate = true
+    mailInfo.userName = "290677893@qq.com" // 发送者邮箱地址
+    mailInfo.password = "sgbozwzhkfvjcaie" //邮箱授权码，不是密码
+    mailInfo.toAddress = toAddress // 接收者邮箱
+    mailInfo.fromAddress = "290677893@qq.com" // 发送者邮箱
+    mailInfo.subject = "自动打卡通知" // 邮件主题
+    val content = if (this == "") {
+        "未监听到打卡成功的通知，请手动登录检查" + System.currentTimeMillis().timestampToDate()
+    } else {
+        this
+    }
+    // 邮件文本
+    mailInfo.content = content
+    return mailInfo
 }
