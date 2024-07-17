06c0899f6e5bacc539af9578f74797f5784bab7b
pengxianhong <290677893@qq.com>
2024-07-17 08:45:48 +0800

    删除无障碍服务，编译1.4.8版本

--- app/src/main/java/com/pengxh/autodingding/utils/CountDownTimerManager.kt
+++ app/src/main/java/com/pengxh/autodingding/utils/CountDownTimerManager.kt
@@ -8,14 +8,10 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LifecycleRegistry
 import androidx.lifecycle.lifecycleScope
-import com.pengxh.autodingding.extensions.createAttachFileMail
 import com.pengxh.autodingding.extensions.createTextMail
-import com.pengxh.autodingding.extensions.sendAttachFileMail
 import com.pengxh.autodingding.extensions.sendTextMail
 import com.pengxh.autodingding.ui.MainActivity
 import com.pengxh.kt.lite.extensions.show
-import com.pengxh.kt.lite.extensions.timestampToCompleteDate
-import com.pengxh.kt.lite.utils.ActivityStackManager
 import com.pengxh.kt.lite.utils.SaveKeyValues
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -71,38 +67,5 @@ class CountDownTimerManager private constructor() : LifecycleOwner {
     fun cancelTimer() {
         timer?.cancel()
         Log.d(kTag, "cancelTimer: 取消超时定时器")
-    }
-
-    fun startTimer(
-        context: Context, millisInFuture: Long, countDownInterval: Long, imagePath: String
-    ) {
-        Log.d(kTag, "startTimer: 开始倒计时")
-        object : CountDownTimer(millisInFuture, countDownInterval) {
-            override fun onTick(millisUntilFinished: Long) {
-                Log.d(kTag, "onTick: ${millisUntilFinished / 1000}")
-            }
-
-            override fun onFinish() {
-                ActivityStackManager.finishAllActivity()
-                val intent = Intent(context, MainActivity::class.java)
-                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-                context.startActivity(intent)
-
-                val emailAddress = SaveKeyValues.getValue(Constant.EMAIL_ADDRESS, "") as String
-                if (emailAddress.isEmpty()) {
-                    "邮箱地址为空".show(context)
-                    return
-                }
-
-                //如果倒计时结束，那么表明没有收到打卡成功的通知，需要将异常日志保存
-                lifecycleScope.launch(Dispatchers.Main) {
-                    "发送打卡结果邮件，请注意查收".show(context)
-                    withContext(Dispatchers.IO) {
-                        "打卡结果如附件，${System.currentTimeMillis().timestampToCompleteDate()}"
-                            .createAttachFileMail(emailAddress, imagePath).sendAttachFileMail()
-                    }
-                }
-            }
-        }.start()
     }
 }
